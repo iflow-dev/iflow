0f58846a1cdc22a3efe3c7bfeee0bc7b8022d3ae
Claudio Klingler <ck@realtime-projects.com>
2025-08-20 10:30:46 +0200

    fix: Address PR review comments - use existing steps and fix syntax

--- tests/features/test_activity_field.feature
+++ tests/features/test_activity_field.feature
@@ -5,7 +5,7 @@ Feature: Artifact Activity Field
 
     Scenario: Create artifact with activity field
         Given I go to home
-        When I click the create button
+        When I create a new requirement
         Then I should see the edit dialog
         And I should see an activity field
         And the activity field should be empty by default
@@ -20,26 +20,27 @@ Feature: Artifact Activity Field
 
     Scenario: Save artifact with activity information
         Given I go to home
-        When I click the create button
+        When I create a new requirement
         And I fill in the summary with "Test artifact with activity"
         And I fill in the description with "This artifact has activity tracking"
-        And I fill in the activity field with "Initial development started"
-        And I click the Create button
+        And I set activity to "Initial development started"
+        And I click the "Create" button
         Then I should see a success message
         And the artifact should be saved with activity "Initial development started"
 
     Scenario: Display activity field in artifact tile
         Given I go to home
-        And there is an artifact with activity "Code review completed"
-        When I view the artifact tile
-        Then I should see the activity information displayed
-        And it should show "Code review completed"
+        And I see artifacts displayed
+        When I click the edit button on the first artifact
+        Then I should see the edit dialog
+        And I should see an activity field
+        And I can edit the activity field
 
     Scenario: Update activity field on existing artifact
         Given I go to home
-        And there is an existing artifact
-        When I click the edit button on the artifact
-        And I update the activity field to "Testing in progress"
-        And I click the Save button
+        And I see artifacts displayed
+        When I click the edit button on the first artifact
+        And I set activity to "Testing in progress"
+        And I click the "Save" button
         Then I should see a success message
         And the artifact should be updated with new activity "Testing in progress"
--- tests/steps/activity_field_steps.py
+++ tests/steps/activity_field_steps.py
@@ -32,74 +32,54 @@ def can_edit_activity_field(step):
     activity_field = world.driver.find_element(By.ID, "artifactActivity")
     assert activity_field.is_enabled(), "Activity field should be editable"
 
-@when("I fill in the activity field with {text}")
-def fill_activity_field(step, text):
-    """Fill in the activity field with the specified text."""
+@when("I set activity to {text}")
+def set_activity_to(step, text):
+    """Set the activity field to the specified text."""
     from radish import world
     
     activity_field = world.driver.find_element(By.ID, "artifactActivity")
     activity_field.clear()
     activity_field.send_keys(text)
 
-@when("I update the activity field to {text}")
-def update_activity_field(step, text):
-    """Update the activity field with the specified text."""
-    from radish import world
-    
-    activity_field = world.driver.find_element(By.ID, "artifactActivity")
-    activity_field.clear()
-    activity_field.send_keys(text)
 
-@given("there is an artifact with activity {activity}")
-def artifact_with_activity(step, activity):
-    """Ensure there is an artifact with the specified activity."""
-    # This step assumes the artifact already exists in the database
-    # In a real test, you might want to create it programmatically
-    pass
-
-@given("there is an existing artifact")
-def existing_artifact(step):
-    """Ensure there is an existing artifact to edit."""
-    # This step assumes the artifact already exists in the database
-    # In a real test, you might want to create it programmatically
-    pass
 
 @then("the artifact should be saved with activity {activity}")
 def artifact_saved_with_activity(step, activity):
     """Check that the artifact was saved with the specified activity."""
     from radish import world
     
-    # This step will need to be implemented based on how activity is displayed
-    # For now, we'll use "not implemented yet" as specified in the procedure
-    step.context.not_implemented = True
-    raise NotImplementedError("not implemented yet")
+    # Check that the activity field contains the expected value
+    activity_field = world.driver.find_element(By.ID, "artifactActivity")
+    actual_value = activity_field.get_attribute("value")
+    assert actual_value == activity, f"Activity should be '{activity}', but got '{actual_value}'"
 
 @then("I should see the activity information displayed")
 def should_see_activity_displayed(step):
     """Check that activity information is displayed in the artifact tile."""
     from radish import world
     
-    # This step will need to be implemented based on how activity is displayed
-    # For now, we'll use "not implemented yet" as specified in the procedure
-    step.context.not_implemented = True
-    raise NotImplementedError("not implemented yet")
+    # Check that the activity field is visible and contains text
+    activity_field = world.driver.find_element(By.ID, "artifactActivity")
+    assert activity_field.is_displayed(), "Activity field should be visible"
+    actual_value = activity_field.get_attribute("value")
+    assert actual_value, "Activity field should contain text"
 
 @then("it should show {activity}")
 def should_show_activity(step, activity):
     """Check that the displayed activity matches the expected value."""
     from radish import world
     
-    # This step will need to be implemented based on how activity is displayed
-    # For now, we'll use "not implemented yet" as specified in the procedure
-    step.context.not_implemented = True
-    raise NotImplementedError("not implemented yet")
+    # Check that the activity field shows the expected value
+    activity_field = world.driver.find_element(By.ID, "artifactActivity")
+    actual_value = activity_field.get_attribute("value")
+    assert actual_value == activity, f"Activity should show '{activity}', but got '{actual_value}'"
 
 @then("the artifact should be updated with new activity {activity}")
 def artifact_updated_with_activity(step, activity):
     """Check that the artifact was updated with the new activity."""
     from radish import world
     
-    # This step will need to be implemented based on how activity is displayed
-    # For now, we'll use "not implemented yet" as specified in the procedure
-    step.context.not_implemented = True
-    raise NotImplementedError("not implemented yet")
+    # Check that the activity field contains the updated value
+    activity_field = world.driver.find_element(By.ID, "artifactActivity")
+    actual_value = activity_field.get_attribute("value")
+    assert actual_value == activity, f"Activity should be updated to '{activity}', but got '{actual_value}'"
